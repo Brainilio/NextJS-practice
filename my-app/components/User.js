bb27ace587a71eb529f642b21ef6b52b696cc10c
B.A. Imambaks Rodrigues <0924777@hr.nl>
2020-08-04 18:11:05 +0200

    jsx style components

--- my-app/components/User.js
+++ my-app/components/User.js
@@ -2,9 +2,21 @@ import React from "react"
 
 const User = (props) => {
 	return (
-		<div>
-			I'm a user called {props.name} and I'm {props.age} years old!
-		</div>
+		<>
+			<div>
+				I'm a user called {props.name} and I'm {props.age} years old!
+			</div>
+			<style jsx>
+				{`
+					div {
+						border: 1px solid #eee;
+						box-shadow: 0 2p 3p #ccc;
+						padding: 20px;
+						text-align: center;
+					}
+				`}
+			</style>
+		</>
 	)
 }
 
